7df940d94c6ace492514b25148df84630ef6f578
DESKTOP-S6GD29K\DEVELOPER <pk404849@gmail.com>
2022-03-18 15:09:19 +0530

    base url changes

--- src/app/service/common.service.ts
+++ src/app/service/common.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +12,17 @@ export class CommonService {
     private httpClient: HttpClient
   ) { }
   //private baseUrl = 'https://room-management-server.herokuapp.com';
-  private baseUrl = 'http://localhost:9000';
+  //private baseUrl = 'http://localhost:9000';
 
   addRoom(roomModel: Object): Observable<any> {
+    
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.post(`${this.baseUrl}/room/add-room`, JSON.stringify(roomModel), httpOptions);
+    return this.httpClient.post(`${environment.baseUrl}/room/add-room`, JSON.stringify(roomModel), httpOptions);
   }
 
   findAllRoom (): Observable<any>{
@@ -30,7 +32,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.get(`${this.baseUrl}/room/find-all-room`,httpOptions);
+    return this.httpClient.get(`${environment.baseUrl}/room/find-all-room`,httpOptions);
   }
 
   findAllRoomWithNullFacility():Observable<any>{
@@ -40,7 +42,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.get(`${this.baseUrl}/room/find-all-room-with-null-facility`,httpOptions);
+    return this.httpClient.get(`${environment.baseUrl}/room/find-all-room-with-null-facility`,httpOptions);
   }
 
   findAllFacility():Observable<any>{
@@ -50,7 +52,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.get(`${this.baseUrl}/facility/find-all-facility`,httpOptions);
+    return this.httpClient.get(`${environment.baseUrl}/facility/find-all-facility`,httpOptions);
   }
 
   addFacility(facilityModel: Object): Observable<any> {
@@ -60,7 +62,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.post(`${this.baseUrl}/facility/add-facility`, JSON.stringify(facilityModel), httpOptions);
+    return this.httpClient.post(`${environment.baseUrl}/facility/add-facility`, JSON.stringify(facilityModel), httpOptions);
   }
   addResident(residentModel: Object): Observable<any> {
     const httpOptions = {
@@ -69,7 +71,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.post(`${this.baseUrl}/resident/add-resident`, JSON.stringify(residentModel), httpOptions);
+    return this.httpClient.post(`${environment.baseUrl}/resident/add-resident`, JSON.stringify(residentModel), httpOptions);
   }
 
   findAllResident(): Observable<any>{
@@ -79,7 +81,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.get(`${this.baseUrl}/resident/find-all-resident`,httpOptions);
+    return this.httpClient.get(`${environment.baseUrl}/resident/find-all-resident`,httpOptions);
   }
 
   findAllWithoutBookedRoom(): Observable<any>{
@@ -89,7 +91,7 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.get(`${this.baseUrl}/room/find-all-without-booked-room`,httpOptions);
+    return this.httpClient.get(`${environment.baseUrl}/room/find-all-without-booked-room`,httpOptions);
   }
 
   checkOutResident(residentId:number):Observable<any>{
@@ -99,6 +101,6 @@ export class CommonService {
         'Accept': 'application/json',
       })
     }
-    return this.httpClient.get(`${this.baseUrl}/resident/check-out-resident/` + residentId,httpOptions);
+    return this.httpClient.get(`${environment.baseUrl}/resident/check-out-resident/` + residentId,httpOptions);
   }
 }
